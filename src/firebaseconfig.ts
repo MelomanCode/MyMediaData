7907691d9e1ccff0fb349ad54291efd2a57f9e47
MelomanCode <131396809+MelomanCode@users.noreply.github.com>
2023-05-10 19:31:36 +0200

    Delete firebaseconfig.ts

--- src/firebaseconfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseconfig: { [key: string]: string } = {
-  apiKey: 'AIzaSyCsBk53_rbDRcvw8ILrBH9PGEu0g5fSq7Q',
-  authDomain: 'mymediadata-11a08.firebaseapp.com',
-  projectId: 'mymediadata-11a08',
-  storageBucket: 'mymediadata-11a08.appspot.com',
-  messagingSenderId: '857477306982',
-  appId: '1:857477306982:web:c644604f660927ccc86ba7',
-};
