aa715f444354c6498ef6c21b37c7e5aaa10a72dc
MelomanCode <dmitryKezik@gmail.com>
2023-07-19 15:25:27 +0200

    v0.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FilmsService } from './services/films.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  constructor(private apiService: FilmsService) {}
-
-  ngOnInit() {}
-}
+export class AppComponent {}
--- src/app/auth/authentication/authentication.component.ts
+++ src/app/auth/authentication/authentication.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.css'],
 })
-export class AuthenticationComponent {
+export class AuthenticationComponent implements OnInit {
   firebaseErrorMessage = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public auth: AngularFireAuth
+  ) {}
+
+  ngOnInit(): void {
+    this.auth.user.subscribe((user) => {
+      if (user) {
+        this.router.navigate(['/admin-panel-list']).then();
+      }
+    });
+  }
 
   tryGoogleLogin(): void {
     this.authService.loginGoogle().then((result) => {
